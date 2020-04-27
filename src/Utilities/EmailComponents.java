17b70eac497e461a6743bb8072b1b463cf98c6f4
MohannadSoliman <61092652+MohannadSoliman@users.noreply.github.com>
2020-04-27 12:44:49 +0200

    Update EmailComponents.java

--- src/Utilities/EmailComponents.java
+++ src/Utilities/EmailComponents.java
@@ -3,7 +3,10 @@ package Utilities;
 public class EmailComponents {
 	String ID;
 	String subject;
+	String bodyText;
 	String time;
 	String sender;
+	SinglyLinkedList receivers = new SinglyLinkedList();
 	String priority;
+	SinglyLinkedList attachments = new SinglyLinkedList();
 }
